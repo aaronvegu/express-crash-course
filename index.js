51565c2b5b041fe0e2c4d259af7a7be03e675b7b
Aaron Vegu <aaron@reckoner-rde.io>
2020-10-19 14:07:02 -0500

    Create a static folder with express to handle any kind of static files inside that folder

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const path = require('path');
 // Init express
 const app = express();
 
-// Creamos un route para manejar (route handler) un request GET en home (/)
-// sintaxis: get('path', callback)
-app.get('/', (req, res) => {
-  // Enviando archivos como respuesta al browser
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// Creamos un folder para archivos estaticos
+app.use(express.static(path.join(__dirname, 'public')));
+/**
+ * Haciendo uso del metodo static() del modulo express nos permite manejar archivos estaticos
+ * que se encuentran dentro de la carpeta que hemos declarado como parametro con join de path,
+ * manejando diferentes tipos de extensiones y archivos de manera automatica
+ */
 
 // Definimos el puerto con opcion a puerto de desarollo del deploy server
 const PORT = process.env.PORT || 5000;
